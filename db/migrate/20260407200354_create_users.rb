dccea6ceec1ea5066d82e1e163a00b8aae30f86a
Dennis Martinez <denniswaltermartinez@gmail.com>
2026-04-07 14:06:06 -0600

    initial auth generator

---
+++ db/migrate/20260407200354_create_users.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false, limit: 255
+      t.string :email, null: false, limit: 255, index: { unique: true }
+      t.string :password_digest, null: false
+
+      t.timestamps
+    end
+  end
+end
