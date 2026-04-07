0e04891e65617c6e3212c92166a72ce27d9233a8
Dennis Martinez <denniswaltermartinez@gmail.com>
2026-04-07 12:36:15 -0600

    Adding GQL on the front end and revamping docker

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,16 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    allow_origins = []
+    allow_origins << "http://localhost:5173" if Rails.env.development?
+
+    origins allow_origins
+
+    resource "/graphql",
+      headers: :any,
+      credentials: true,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
