ca974eb32e373176dff0974ed887abbef5f29548
Dennis Martinez <denniswaltermartinez@gmail.com>
2026-04-09 18:10:27 -0600

    fixing likeable optimistic response returning the wrong object

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -9,7 +9,6 @@ module Types
     field :can_destroy, Boolean, null: false, description: 'Whether the current user can destroy this post.'
     field :content, String, null: false, description: 'The content of the post.'
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false, description: 'The time when the post was created.'
-    field :is_liked_by_current_user, Boolean, null: false, description: 'Whether the current user has liked this post.'
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false,
                                                         description: 'The time when the post was last updated.'
     field :user, Types::UserType, null: false, description: 'The user who created the post.'
@@ -33,8 +32,8 @@ module Types
       object.can_destroy?(context[:current_user])
     end
 
-    def is_liked_by_current_user
-      context[:current_user].liked_posts.exists?(id: object.id)
+    def current_user_like
+      object.likes.find_by(user_id: context[:current_user].id)
     end
   end
 end
